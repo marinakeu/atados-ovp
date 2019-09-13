b5565f1faeabe936fcdb806bdadbfe0c4ad7632e
Leonardo Arroyo <contato@leonardoarroyo.com>
2019-09-13 17:55:08 -0300

    Update gdd content flow

--- api/channels/gdd/content_flow.py
+++ api/channels/gdd/content_flow.py
@@ -12,21 +12,23 @@ from ovp.apps.channels.content_flow import NoContentFlow
 from ovp.apps.channels.content_flow import CFM
 
 class GDDContentFlow(BaseContentFlow):
-  source = "gdd"
-  destination = "default"
+  source = "default"
+  destination = "gdd"
 
   def __init__(self):
     try:
-      self.category_id = Category.objects.get(slug="instituto-center-norte").pk
+      self.category_id = Category.objects.get(slug="export-to-gdd-2020").pk
     except:
       self.category_id = None
 
   def get_filter_searchqueryset_q_obj(self, model_class):
     if model_class == Project:
-      return SQ(organization_categories=self.category_id)
+      #return SQ(organization_categories=self.category_id)
+      return SQ()
 
     elif model_class == Organization:
-      return SQ(categories=self.category_id)
+      #return SQ(categories=self.category_id)
+      return SQ()
 
     raise NoContentFlow
 
@@ -34,12 +36,15 @@ class GDDContentFlow(BaseContentFlow):
     if model_class in [Cause, Skill]:
       return Q()
     elif model_class == Project:
-      return Q(organization__categories=self.category_id)
+      #return Q(organization__categories=self.category_id)
+      return Q()
     elif model_class == Organization:
-      return Q(categories=self.category_id)
+      #return Q(categories=self.category_id)
+      return Q()
     elif model_class == Apply:
-      return Q(project__organization__categories=self.category_id)
+      #return Q(project__organization__categories=self.category_id)
+      return Q()
 
     raise NoContentFlow
 
-#CFM.add_flow(ICNContentFlow())
+CFM.add_flow(GDDContentFlow())

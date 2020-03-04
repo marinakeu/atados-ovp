29f6316a3fbe11aff6017d960a380d2c08ad64f0
Vinicius Pacheco Furtado <vin175pacheco@gmail.com>
2020-03-04 19:12:20 -0300

    Fix duplicate class name on default content_flow

--- api/channels/default/content_flow.py
+++ api/channels/default/content_flow.py
@@ -2,10 +2,10 @@ from os import getenv
 from ovp.apps.channels.content_flow import BaseContentFlow
 from ovp.apps.channels.content_flow import CFM
 
-class BaseContentFlow(BaseContentFlow):
+class BaseChannelContentFlow(BaseContentFlow):
   source = "default"
   destination = "base"
 
 
 if getenv('ENABLE_BASE_CONTENT_FLOW'):
-  CFM.add_flow(BaseContentFlow())
+  CFM.add_flow(BaseChannelContentFlow())

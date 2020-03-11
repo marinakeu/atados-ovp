b9f99cd5d65a6a7c9a41ec64d613abf98f1e3b8c
Leonardo Arroyo <contato@leonardoarroyo.com>
2020-03-10 22:42:11 -0300

    Update django-ovp

--- api/fixtures.sh
+++ api/fixtures.sh
@@ -1,20 +1,26 @@
 echo "Core: "
-python manage.py loaddata core.json
-echo "Channel: "
-python manage.py loaddata channel.json
+python manage.py loaddata addresstypes components address posts
+
+echo "Images: "
+python manage.py loaddata images
+
 echo "Users: "
-python manage.py loaddata users.json
+python manage.py loaddata users profiles
+
 echo "Organizations: "
-python manage.py loaddata organizations.json
+python manage.py loaddata organizations
+
 echo "Project: "
-python manage.py loaddata project.json
-echo "VolunteerRole: "
-python manage.py loaddata volunteerrole.json
-echo "Job: "
-python manage.py loaddata job.json
-echo "JobDate: "
-python manage.py loaddata jobdate.json
-echo "Work: "
-python manage.py loaddata work.json
-echo "Apply: "
-python manage.py loaddata apply.json
+python manage.py loaddata projects roles works jobs jobdates applies categories projectbookmarks
+
+echo "Catalogue: "
+python manage.py loaddata catalogue
+
+# Donations
+# TODO: Subscription (finish subscriptions on donations app first)
+echo "Donations: "
+python manage.py loaddata sellers transactions
+
+# Faq
+echo "FAQ: "
+python manage.py loaddata faqcategory faq

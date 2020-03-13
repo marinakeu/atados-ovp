57aa509388d7f71a05d9581438c28b7b3251fc5d
Leonardo Arroyo <contato@leonardoarroyo.com>
2020-03-12 21:15:43 -0300

    Add signal to automatically bind brazillian gdd projects to dba-2020 category

--- api/channels/gdd/signals.py
+++ api/channels/gdd/signals.py
@@ -3,6 +3,7 @@ from ovp.apps.projects.models import Project
 from ovp.apps.core.models import GoogleAddress
 from django.db.models.signals import post_save
 from channels.gdd.emails import GDDMail
+from ovp.apps.projects.models.category import Category
 
 User = get_user_model()
 
@@ -13,7 +14,9 @@ def send_email_to_manager(sender, *args, **kwargs):
   instance = kwargs["instance"]
 
   if instance.channel.slug == "gdd" and kwargs["created"] and not kwargs["raw"]:
-    address = GoogleAddress(pk=instance.address.pk)
+    if not instance.address:
+        return None
+    address = GoogleAddress.objects.get(pk=instance.address.pk)
     country = address.address_components.filter(types__name='country').first()
     if country:
         country = country.short_name
@@ -24,3 +27,21 @@ def send_email_to_manager(sender, *args, **kwargs):
     for manager in managers:
       GDDMail(manager, async_mail=True).sendProjectCreatedToCountryManager({'project': instance})
 post_save.connect(send_email_to_manager, sender=Project)
+
+def add_to_gdd_brasil_category(sender, *args, **kwargs):
+  """
+  Add gdd projects created in gdd channel to dba-2020 category
+  """
+  instance = kwargs["instance"]
+
+  if instance.channel.slug == "gdd" and not kwargs["raw"]:
+    if not instance.address:
+        return None
+
+    category = Category.objects.get(slug="dba-2020")
+    address = GoogleAddress.objects.get(pk=instance.address.pk)
+    country = address.address_components.filter(types__name='country').first()
+
+    if country and country.short_name == "BR":
+        instance.categories.add(category)
+post_save.connect(add_to_gdd_brasil_category, sender=Project)

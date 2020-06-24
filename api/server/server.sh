9fa7a0a051a21ae07bfbb26d54b7928df4f4ead7
Leonardo Arroyo <contato@leonardoarroyo.com>
2020-06-23 22:30:39 -0300

    Update celery app in server.sh

--- api/server/server.sh
+++ api/server/server.sh
@@ -2,7 +2,7 @@
 
 PRJ=`pwd | rev | cut -d "/" -f3 | rev`
 celery multi stopwait worker1 --pidfile="/tmp/celery.%n.pid"
-celery multi start worker1 -A server \
+celery multi start worker1 -A server.celery_tasks \
     --logfile="$HOME/api/logs/celery/%n%I.log" \
     --pidfile="/tmp/celery.%n.pid"
 gunicorn server.wsgi:application -w 5 --timeout 300 --limit-request-line 16382 --bind unix:/tmp/api.$PRJ.socket

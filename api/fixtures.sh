e9298264c363860996b726612ac02482ba6c96b8
Leonardo Arroyo <contato@leonardoarroyo.com>
2020-07-14 18:54:12 -0300

    Update fixtures

--- api/fixtures.sh
+++ api/fixtures.sh
@@ -5,32 +5,32 @@ script_full_path=$(dirname "$0")
 
 cd $script_full_path
 
-#echo "Core: "
-#python manage.py loaddata addresstypes components address posts
-#
-#echo "Images: "
-#python manage.py loaddata images
-#
-#echo "Users: "
-#python manage.py loaddata users profiles
-#
-#echo "Organizations: "
-#python manage.py loaddata organizations
-#
-#echo "Project: "
-#python manage.py loaddata projects roles works jobs jobdates applies categories projectbookmarks
-#
-#echo "Catalogue: "
-#python manage.py loaddata catalogue
-#
-## Donations
-## TODO: Subscription (finish subscriptions on donations app first)
-#echo "Donations: "
-#python manage.py loaddata sellers transactions
-#
-## Faq
-#echo "FAQ: "
-#python manage.py loaddata faqcategory faq
+echo "Core: "
+python manage.py loaddata addresstypes components address posts
+
+echo "Images: "
+python manage.py loaddata images
+
+echo "Users: "
+python manage.py loaddata users profiles
+
+echo "Organizations: "
+python manage.py loaddata organizations
+
+echo "Project: "
+python manage.py loaddata projects roles works jobs jobdates applies categories projectbookmarks
+
+echo "Catalogue: "
+python manage.py loaddata catalogue
+
+# Donations
+# TODO: Subscription (finish subscriptions on donations app first)
+echo "Donations: "
+python manage.py loaddata sellers transactions
+
+# Faq
+echo "FAQ: "
+python manage.py loaddata faqcategory faq
 
 echo "Notifybox: "
 if [ -z ${AWS_ACCESS_KEY_ID+x} ] || [ -z ${AWS_SECRET_ACCESS_KEY+x} ] || [ -z ${AWS_DEFAULT_REGION+x} ] ;
